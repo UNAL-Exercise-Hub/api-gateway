495ef23b3bd188e0198c06ac5e9b7eb8e7571093
jbautistas <jbautistas@unal.edu.co>
2023-11-09 10:06:22 -0500

    "refactor: users"

--- ms_querys/users.js
+++ ms_querys/users.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import "dotenv/config";
 
 export const users_func_querys = `
-  allusers: [User]
+  allusers: UserMessage
+  userByEmail(email: String!): IdMessage
   loginUser(
     email: String
     password: String
@@ -13,11 +14,12 @@ export const users_func_mutations = `
   addUser(
     nombres: String
     apellidos: String
+    documento: String
     fecha_nacimiento: String
     sexo: String
     cel: String
     email: String
-  ): String
+  ): Message
 
   registerUser(
     nombres: String
@@ -29,13 +31,32 @@ export const users_func_mutations = `
     password: String
   ): Message 
 
+  updateUser(
+    id_usuario: ID!
+    nombres: String
+    apellidos: String
+    documento: String
+    fecha_nacimiento: String
+    sexo: String
+    cel: String
+  ): Message
+
+  deleteUser(ID: ID!): Message
 `;
 
 export const users_squemas = `
+  type UserMessage {
+    users: [User]  
+  }
+  type IdMessage {
+    message: String
+    id_usuario: String
+  }
   type User {
     id_usuario: ID!
     nombres: String
     apellidos: String
+    documento: String
     fecha_nacimiento: String
     sexo: String
     cel: Float
@@ -50,26 +71,36 @@ export const users_querys = {
     );
     return result.data;
   },
+  userByEmail: async (_, { email }) => {
+    try {
+      const result = await axios.get(
+        `http://${process.env.NAME_USERS}:${process.env.PORT_USERS}/user/byEmail?email=${email}`
+      );
+      return result.data;
+    } catch (error) {
+      return { message: "Usuario no encontrado" };
+    }
+  },
   loginUser: async (_, args) => {
     const result = await axios.get(
       `http://${process.env.NAME_USERS}:${process.env.PORT_USERS}/user`
     );
     var id_user = null;
     for (let i = 0; i < result.data.length; i++) {
-      if (result.data[i].email == args['email'] ) {
+      if (result.data[i].email == args["email"]) {
         id_user = result.data[i].id_usuario;
       }
     }
     const result_2 = await axios.get(
-        `http://${process.env.NAME_AUTH}:${process.env.PORT_AUTH}/login/${id_user}`
-        );
-    return {message: "Usuario logeado correctamente"};
-  }
+      `http://${process.env.NAME_AUTH}:${process.env.PORT_AUTH}/login/${id_user}`
+    );
+    return { message: "Usuario logeado correctamente" };
+  },
 };
 
 export const users_mutations = {
   addUser: async (_, args) => {
-    args['cel'] = parseFloat(args['cel']);
+    args["cel"] = parseFloat(args["cel"]);
     const result = await axios.post(
       `http://${process.env.NAME_USERS}:${process.env.PORT_USERS}/user`,
       args
@@ -77,18 +108,41 @@ export const users_mutations = {
     return result.data;
   },
   registerUser: async (_, args) => {
-    args['cel'] = parseFloat(args['cel']);
+    args["cel"] = parseFloat(args["cel"]);
     await axios.post(
       `http://${process.env.NAME_USERS}:${process.env.PORT_USERS}/user`,
       args
     );
 
     await axios.post(
-          `http://${process.env.NAME_AUTH}:${process.env.PORT_AUTH}/login/`,
-        {
-          "UserEmail": args['email'],
-          "UserPasswordHash": args['password']}
+      `http://${process.env.NAME_AUTH}:${process.env.PORT_AUTH}/login/`,
+      {
+        UserEmail: args["email"],
+        UserPasswordHash: args["password"],
+      }
+    );
+    return { message: "Usuario registrado correctamente" };
+  },
+  updateUser: async (_, args) => {
+    try {
+      args["cel"] = parseFloat(args["cel"]);
+      const result = await axios.put(
+        `http://${process.env.NAME_USERS}:${process.env.PORT_USERS}/user/${args["id_usuario"]}`,
+        args
       );
-    return {message: "Usuario registrado correctamente"};
-  }  
+      return result.data;
+    } catch (error) {
+      return { message: "Error al actualizar el usuario" };
+    }
+  },
+  deleteUser: async (_, { ID }) => {
+    try{
+    const result = await axios.delete(
+      `http://${process.env.NAME_USERS}:${process.env.PORT_USERS}/user/${ID}`
+    );
+    return result.data;
+    } catch (error) {
+      return { message: "Error al eliminar el usuario" };
+    }
+  },
 };
